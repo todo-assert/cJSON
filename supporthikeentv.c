67d0431805037b5878d7986b13962280d6647554
assert <1218715400@qq.com>
2019-09-23 11:08:08 +0800

    修改json整合工具整合方式

--- supporthikeentv.c
+++ supporthikeentv.c
@@ -42,6 +42,14 @@ static int add_config(char *search_path, cJSON *item)
 	return size;
 }
 
+extern int print_usage(char *target);
+int print_usage(char *target)
+{
+	printf(
+		"%s common_dirent config_dirent\n\n", target
+		);
+	return 0;
+}
 
 int main(int argc, char **argv) 
 {
@@ -51,13 +59,18 @@ int main(int argc, char **argv)
 	struct dirent *dirent;
 	char path[1024] ;
 	size_t size;
+	int itemsize, i;
+	size_t len;
 	char *pbuf;
 	__attribute__((unused)) char *search_path = argv[1];
 	__attribute__((unused)) char *inputfile = argv[2];
+	__attribute__((unused)) char *input_path = argv[2];
 	__attribute__((unused)) char *genfile = argv[3];
 	__attribute__((unused)) char *out;
-	__attribute__((unused)) cJSON *top, *root, *support, *item, *input, *object;
+	FILE *fp;
+	cJSON *top, *root, *support, *item, *input, *object, *config, *childobj;
 	if( argc < 2 ) {
+		print_usage(argv[0]);
 		return ret;
 	}
 	dir = opendir(search_path);
@@ -67,6 +80,7 @@ int main(int argc, char **argv)
 	top = 	cJSON_CreateObject();
 	root = 	cJSON_CreateObject();
 	support = 	cJSON_CreateObject();
+	config = 	cJSON_CreateObject();
 	while((dirent = readdir(dir))!=NULL) {
 		if( strcmp(".", dirent->d_name) == 0 || strcmp("..", dirent->d_name) == 0 ) continue ;
 		if(DT_ISDIR(dirent->d_type)) {
@@ -77,6 +91,7 @@ int main(int argc, char **argv)
 			cJSON_AddItemToObject(support, dirent->d_name, item);
 		}
 	}
+	closedir(dir);
 
 	cJSON_AddItemReferenceToObject(top, "company", cJSON_CreateString("SHENZHEN HIKEEN TECHNOLOGY CO.,LTD"));
 	cJSON_AddItemReferenceToObject(top, "version", cJSON_CreateString("CJSON " LIBVERSION));
@@ -85,8 +100,45 @@ int main(int argc, char **argv)
 	cJSON_AddItemReferenceToObject(top, "email", cJSON_CreateString("1218715400@qq.com"));
 	cJSON_AddItemToObject(top, "hikeentv", root);
 	cJSON_AddItemToObject(root, "support", support);
+	cJSON_AddItemToObject(root, "config", config);
 
-	FILE *fp;
+// add all json
+	dir = opendir(input_path);
+	if( dir == NULL ) {
+		return ret;
+	}
+	while((dirent = readdir(dir))!=NULL) {
+		if( strcmp(".", dirent->d_name) == 0 || strcmp("..", dirent->d_name) == 0 ) continue ;
+		if(DT_ISDIR(dirent->d_type)) continue ;
+		len = strlen(dirent->d_name);
+		if( len > 5 && strcasecmp(".json", &dirent->d_name[len-5]) == 0 ) {
+			printf("open %s\n", dirent->d_name);
+			sprintf(path, "%s/%s", input_path, dirent->d_name);
+			if(NULL != (fp = fopen(path, "r"))) {
+				fseek(fp, 0, SEEK_END);
+				size = (size_t ) ftell(fp) + 1;
+				fseek(fp, 0, SEEK_SET);
+				pbuf = (char *)malloc(size);
+				if(pbuf != NULL && (0 != fread(pbuf, 1, size, fp))) {
+					input=cJSON_Parse(pbuf);
+					if(input) {
+						object = cJSON_GetObjectItem(input,"config");
+						itemsize = cJSON_GetArraySize(object);
+						for(i=0;i<itemsize;i++) {
+							childobj = cJSON_GetArrayItem(object, i);
+							if(childobj)
+								cJSON_AddItemToArray(config, childobj);
+						}
+					}
+					free(pbuf);
+				}
+				fclose(fp);
+			}
+		}
+	}
+	closedir(dir);
+
+/*
 	if(NULL != (fp = fopen(inputfile, "r"))) {
 		fseek(fp, 0, SEEK_END);
 		size = (size_t ) ftell(fp) + 1;
@@ -102,7 +154,7 @@ int main(int argc, char **argv)
 		}
 		fclose(fp);
 	}
-
+*/
 	out=cJSON_Print(top);
 	if( genfile == NULL )
 		printf("\n%s\n", out);
